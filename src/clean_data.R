fe3995051a0b3582cf422f6501930dfac866cd3a
Kristoffer Klevjer <klevjer@gmail.com>
2022-09-06 13:04:04 +0200

    init

---
+++ src/clean_data.R
@@ -0,0 +1,13 @@
+####################################################################################################
+# Cleans and summaries beads data (prep for analyses)                                              #
+# Input: CSV files from each experiment (and demographics)                                         #
+# Output: CSV-files containing cleaned beads, and collected demographics                           #
+# Kristoffer Klevjer - github.com/klvr - klevjer(a)gmail.com                                       #
+####################################################################################################
+
+# 01 Preparation -----------------------------------------------------------------------------------
+
+# Clean up workspare
+rm(list = ls())
+
+# 02 Load data -------------------------------------------------------------------------------------
