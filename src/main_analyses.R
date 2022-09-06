06e2d8749f6470e38822cccecf052a40b739b102
Kristoffer Klevjer <klevjer@gmail.com>
2022-09-06 16:21:33 +0200

    init

---
+++ src/main_analyses.R
@@ -0,0 +1,15 @@
+####################################################################################################
+# Analyses for the effects of various beads conditions                                             #
+# Input: CSV files from each experiment (processed)                                                #
+# Output: Relevant analyses for article (in workspace)                                             #
+# NB: Seperate script for figures / graphics                                                       #
+# Kristoffer Klevjer - github.com/klvr - klevjer(a)gmail.com                                       #
+####################################################################################################
+
+# 01 Preparation -----------------------------------------------------------------------------------
+
+# Clean up workspace
+rm(list = ls())
+
+# 02 Load data -------------------------------------------------------------------------------------
+
